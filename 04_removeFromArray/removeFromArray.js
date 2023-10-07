37b1fbdc8f1e17f93b2e8eca1de4a457598f9541
Adam Symonds <symonds.adam@gmail.com>
2023-10-08 00:59:56 +1100

    Started 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...entries) {
+  let response = arr;
 
+  response = arr.filter((item) => item !== entries);
+  console.log(response);
+  return response;
 };
 
 // Do not edit below this line
