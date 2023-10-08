2c1fed22dcb83719e5de3e98b0cca30281a38ec1
Adam Symonds <symonds.adam@gmail.com>
2023-10-08 22:07:37 +1100

    Temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  celsiusTemp = (parseInt(temp) - 32) * (5 / 9);
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  fahrenheitTemp = temp * (9 / 5) + 32;
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
