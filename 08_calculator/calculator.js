6fac6ca554651dc6b2685efee43650f763864a53
Adam Symonds <symonds.adam@gmail.com>
2023-10-13 16:53:01 +1100

    COmpelted 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  return numArray.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  return numArray.reduce((total, num) => {
+    return total === 0 ? num : total * num;
+  }, 0);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum = 1;
+  for (let i = 1; i < num; i++) {
+    sum += sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
