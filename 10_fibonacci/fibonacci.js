96105e44789350313b4bbd3d2fa1f1490463c237
Adam Symonds <symonds.adam@gmail.com>
2023-10-13 22:37:46 +1100

    10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let firstNum = 1;
+  let secondNum = 1;
 
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 0; i < num - 2; i++) {
+    let hold = secondNum;
+    secondNum += firstNum;
+    firstNum = hold;
+  }
+  return secondNum;
 };
 
 // Do not edit below this line
