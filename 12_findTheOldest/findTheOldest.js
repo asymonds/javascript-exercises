af9210f0d495ff2a70c111fc511759db601911d0
Adam Symonds <symonds.adam@gmail.com>
2023-10-13 23:01:21 +1100

    12 Find Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  // const sortedArr = arr.sort((a, b) => {
+  //   return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth
+  //     ? -1
+  //     : 1;
+  // });
+  const oldest = arr.reduce((a, b) => {
+    if (!a["yearOfDeath"]) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (!b["yearOfDeath"]) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth
+      ? { ...a }
+      : { ...b };
+  }, {});
+  return oldest;
+  console.log(oldest.name);
 };
 
 // Do not edit below this line
