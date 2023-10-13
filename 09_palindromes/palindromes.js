f30ab2ff3029d456b15ce750ee86c3aa251e39fe
Adam Symonds <symonds.adam@gmail.com>
2023-10-13 22:20:34 +1100

    09 palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const origWord = word.toLowerCase().split("").filter(removePunctuation);
+  const reverseWord = [...origWord].reverse("").join("");
+  console.log(`${origWord.join("")} vs ${reverseWord}`);
+  return origWord.join("") === reverseWord;
 };
+
+function removePunctuation(item) {
+  if (item === "!" || item === " " || item === "," || item === ".") {
+    return false;
+  }
+  return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
